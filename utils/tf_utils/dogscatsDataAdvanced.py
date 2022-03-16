0c8dfe9d0ce57fc1aafa8ac9a27600bf87f9f4f5
Christoph Kempkes <Kempkes.Christoph@gmail.com>
2022-03-16 10:06:41 +0100

    CATSDOGS Update

--- utils/tf_utils/dogscatsDataAdvanced.py
+++ utils/tf_utils/dogscatsDataAdvanced.py
@@ -3,12 +3,15 @@ import numpy as np
 import cv2
 from skimage import transform
 
+import tensorflow as tf
 from tensorflow.keras.utils import to_categorical
-from typing import Tuple
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.layers.experimental.preprocessing import RandomRotation # Rotieren eines Bildes (Data Augmentation)
+from tensorflow.keras.layers.experimental.preprocessing import RandomTranslation # Verschieben eines Bildes (Data Augmentation)
+from tensorflow.keras.layers.experimental.preprocessing import RandomZoom # Zoom Bild (Data Augmentation)
+from tensorflow.keras.layers.experimental.preprocessing import Rescaling # Quasi MinMaxScaling
 from sklearn.model_selection import train_test_split
+from tensorflow.keras.models import Sequential
 
-# %%
 DATA_DIR = os.path.join("C:/Selbststudium/Zubehoer/Cats_and_dogs")
 X_FILE_PATH = os.path.join(DATA_DIR, "x.npy")
 Y_FILE_PATH = os.path.join(DATA_DIR, "y.npy")
@@ -98,36 +101,47 @@ class DOGSCATS:
         self.height = self.x_train.shape[2]
         self.depth = self.x_train.shape[3]
         self.img_shape = (self.width, self.height, self.depth)
+        # tf.data.Datasets: Daten aus den Numpy-Arrays holen und ins Dataset packen
+        self.train_dataset = tf.data.Dataset.from_tensor_slices((self.x_train, self.y_train))
+        self.test_dataset = tf.data.Dataset.from_tensor_slices((self.x_test, self.y_test))
+        self.val_dataset = tf.data.Dataset.from_tensor_slices((self.x_val, self.y_val))
+        # Data Augmentation
+        self.train_dataset = self._prepare_dataset(self.train_dataset, shuffle=True, augment=True)
+        self.test_dataset = self._prepare_dataset(self.test_dataset) # Kein Data Augmentation 
+        self.val_dataset = self._prepare_dataset(self.val_dataset) # Kein Data Augmentation 
 
-    def get_train_set(self) -> Tuple[np.ndarray, np.ndarray]:
+    def get_train_set(self) -> tf.data.Dataset:
         return self.train_dataset
     
-    def get_test_set(self) -> Tuple[np.ndarray, np.ndarray]:
+    def get_test_set(self) -> tf.data.Dataset:
         return self.test_dataset
 
-    def get_val_set(self) -> Tuple[np.ndarray, np.ndarray]:
+    def get_val_set(self) -> tf.data.Dataset:
         return self.val_dataset
+    
+    @staticmethod
+    def _build_data_augmentation() -> Sequential:
+        model = Sequential()
+        model.add(RandomRotation(factor=0.08))
+        model.add(RandomTranslation(height_factor=0.08, width_factor=0.08))
+        model.add(RandomZoom(height_factor=0.08, width_factor=0.08))
+        return model
 
-    def data_augmentation(self, augment_size: int = 5_000) -> None:
-        image_generator = ImageDataGenerator(
-            rotation_range = 5,
-            zoom_range=0.08,
-            width_shift_range=0.08,
-            height_shift_range=0.08
-        )
-        # Fit the data generator
-        image_generator.fit(self.x_train, augment=True)
-        # Get random train images for the data augmentation
-        rand_idxs = np.random.randint(self.train_size, size=augment_size)
-        x_augmented = self.x_train[rand_idxs].copy()
-        y_augmented = self.y_train[rand_idxs].copy()
-        x_augmented = image_generator.flow(
-            x_augmented,
-            np.zeros(augment_size),
-            batch_size=augment_size,
-            shuffle=False
-        ).next()[0]
-        # Append the augmented images to the train set
-        self.x_train = np.concatenate((self.x_train, x_augmented))
-        self.y_train = np.concatenate((self.y_train, y_augmented))
-        self.train_size = self.x_train.shape[0]
+    def _prepare_dataset(
+        self,
+        dataset: tf.data.Dataset,
+        shuffle: bool = False,
+        augment: bool = False
+    ) -> tf.data.Dataset:
+        if shuffle:
+            dataset = dataset.shuffle(buffer_size=1_000)
+        dataset = dataset.batch(batch_size=self.batch_size)
+        if augment:
+            data_augmentation_model = self._build_data_augmentation()
+            dataset = dataset.map(
+                map_func=lambda x, y: (data_augmentation_model(x, training=False), y),
+                num_parallel_calls=tf.data.experimental.AUTOTUNE
+            )
+        return dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+
+data = DOGSCATS()
